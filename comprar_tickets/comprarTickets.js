ceb27de488dd116213d30d053404b5757e38b246
Joaquin Esteban Bueno <100166316+sanyojoa@users.noreply.github.com>
2023-05-25 19:36:00 -0300

    accion boton borrar comprarTickets.js

--- comprar_tickets/comprarTickets.js
+++ comprar_tickets/comprarTickets.js
@@ -1,1 +1,6 @@
-console.log("Ahora sii")
+let botonBorrar = document.getElementById("boton-borrar");
+let inputs = document.querySelectorAll("input")
+
+botonBorrar.addEventListener('click', ()=>{
+    inputs.forEach(input => input.value = '');
+});
