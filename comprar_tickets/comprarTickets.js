c8b3fb67810a4dc9ac32c75d63bca4dc3a899ee5
Joaquin Esteban Bueno <100166316+sanyojoa@users.noreply.github.com>
2023-06-02 16:56:40 -0300

    Modal de resumen en html

--- comprar_tickets/comprarTickets.js
+++ comprar_tickets/comprarTickets.js
@@ -1,6 +1,37 @@
 let botonBorrar = document.getElementById("boton-borrar");
-let inputs = document.querySelectorAll("input")
+let inputs = document.querySelectorAll("input");
+let categorias = document.getElementById("Categoria");
+
+promocion = 1;
+categorias.addEventListener('click', ()=>{
+    valorCategorias = categorias.value;
+    if(valorCategorias == 1){
+        promocion = 0.2;
+        return promocion;
+    }
+    if(valorCategorias == 0){
+        promocion = 1;
+        return promocion;
+    }
+    if(valorCategorias == 2){
+        promocion = 0.5;
+        return promocion;
+    }
+    if(valorCategorias == 3){
+        promocion = 0.85;
+        return promocion;
+    }
+})
+
 
 botonBorrar.addEventListener('click', ()=>{
     inputs.forEach(input => input.value = '');
 });
+
+
+
+function cantidadBoletos(valor){
+    valor = parseInt(Math.round(valor))
+    document.getElementById('total').innerHTML = 'Total a Pagar: $ ' + 200 * valor * promocion;
+}
+
