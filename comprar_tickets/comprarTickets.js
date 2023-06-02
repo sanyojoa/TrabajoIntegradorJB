b50850f436933d15cab66eecf3deb93029711484
Joaquin Esteban Bueno <100166316+sanyojoa@users.noreply.github.com>
2023-06-02 18:26:43 -0300

    Funcion resumen para modal

--- comprar_tickets/comprarTickets.js
+++ comprar_tickets/comprarTickets.js
@@ -1,25 +1,22 @@
 let botonBorrar = document.getElementById("boton-borrar");
 let inputs = document.querySelectorAll("input");
 let categorias = document.getElementById("Categoria");
+let botonResumen = document.getElementById("boton-resumen");
 
-promocion = 1;
+
 categorias.addEventListener('click', ()=>{
     valorCategorias = categorias.value;
     if(valorCategorias == 1){
         promocion = 0.2;
-        return promocion;
     }
     if(valorCategorias == 0){
         promocion = 1;
-        return promocion;
     }
     if(valorCategorias == 2){
         promocion = 0.5;
-        return promocion;
     }
     if(valorCategorias == 3){
         promocion = 0.85;
-        return promocion;
     }
 })
 
@@ -31,7 +28,29 @@ botonBorrar.addEventListener('click', ()=>{
 
 
 function cantidadBoletos(valor){
-    valor = parseInt(Math.round(valor))
-    document.getElementById('total').innerHTML = 'Total a Pagar: $ ' + 200 * valor * promocion;
+    if(valor < 0){
+        alert("La cantidad de boletos no puede ser nulo o menor a 0");
+    }
+    valor = parseInt(Math.round(valor));
+    cantidad = valor;
+}
+
+
+botonResumen.addEventListener('click', ()=>{
+    let vacio = 0;
+    inputs.forEach( input =>{ if(input.value == ''){
+        vacio++;
+    }});
+    if(vacio > 0){
+        alert("Completa los campos vacios");
+        document.getElementById('totales').innerHTML = 'Datos insuficientes para realizar la compra';
+    }else{
+        document.getElementById('total').innerHTML = 'Total a Pagar: $ ' + 200 * promocion * cantidad; 
+        document.getElementById('totales').innerHTML = 'Total a Pagar: $ ' + 200 * promocion * cantidad; 
+    }
+});
+
+function refrescarPagina(){
+    location.reload();
 }
 
