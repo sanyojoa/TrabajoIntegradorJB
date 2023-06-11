5aaae4624485e76fc1943c8e5842f3e0f72365e2
Joaquin Esteban Bueno <100166316+sanyojoa@users.noreply.github.com>
2023-06-11 19:29:12 -0300

    Eventos Cartas comprarTickets.js

--- comprar_tickets/comprarTickets.js
+++ comprar_tickets/comprarTickets.js
@@ -2,7 +2,24 @@ let botonBorrar = document.getElementById("boton-borrar");
 let inputs = document.querySelectorAll("input");
 let categorias = document.getElementById("Categoria");
 let botonResumen = document.getElementById("boton-resumen");
+let cartaA = document.getElementById("carta-A");
+let cartaB = document.getElementById("carta-B");
+let cartaC = document.getElementById("carta-C");
 
+cartaA.addEventListener('click', ()=>{
+    document.getElementById("Categoria").selectedIndex="1";
+    promocion = 0.2;
+})
+
+cartaB.addEventListener('click', ()=>{
+    document.getElementById("Categoria").selectedIndex="2";
+    promocion = 0.5;
+})
+
+cartaC.addEventListener('click', ()=>{
+    document.getElementById("Categoria").selectedIndex="3";
+    promocion = 0.85;
+})
 
 categorias.addEventListener('click', ()=>{
     valorCategorias = categorias.value;
@@ -26,7 +43,17 @@ botonBorrar.addEventListener('click', ()=>{
     document.getElementById('total').innerHTML = 'Total a Pagar: $ ';
 });
 
+function nombreComprador(nombre){
+    compradorNombre = nombre;    
+}
 
+function apellidoComprador(apellido){
+    compradorApellido = apellido;    
+}
+
+function correoComprador(correo){
+    compradorCorreo = correo;    
+}
 
 function cantidadBoletos(valor){
     if(valor < 0){
@@ -47,7 +74,10 @@ botonResumen.addEventListener('click', ()=>{
         document.getElementById('totales').innerHTML = 'Datos insuficientes para realizar la compra';
     }else{
         document.getElementById('total').innerHTML = 'Total a Pagar: $ ' + 200 * promocion * cantidad; 
-        document.getElementById('totales').innerHTML = 'Total a Pagar: $ ' + 200 * promocion * cantidad; 
+        document.getElementById('totales').innerHTML = `Nombre: ${compradorNombre} </br>
+                                                        Apellido: ${compradorApellido} </br>
+                                                        Correo: ${compradorCorreo} </br>
+                                                        Tickets: ${cantidad} </br>Total a Pagar: $ ` + 200 * promocion * cantidad;
     }
 });
 
